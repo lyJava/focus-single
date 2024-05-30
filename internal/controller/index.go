d31905ded3290bf21f840f3702887b31c7754c73
菜哥 <745876299@qq.com>
2024-05-30 13:53:30 +0800

    增加日志

--- internal/controller/index.go
+++ internal/controller/index.go
@@ -2,18 +2,20 @@ package controller
 
 import (
 	"context"
+	"log"
 
 	"focus-single/api/v1"
 	"focus-single/internal/model"
 	"focus-single/internal/service"
 )
 
-// 首页接口
+// Index 首页接口
 var Index = cIndex{}
 
 type cIndex struct{}
 
 func (a *cIndex) Index(ctx context.Context, req *v1.IndexReq) (res *v1.IndexRes, err error) {
+	log.Printf("首页类型:%s", req.Type)
 	if getListRes, err := service.Content().GetList(ctx, model.ContentGetListInput{
 		Type:       req.Type,
 		CategoryId: req.CategoryId,
