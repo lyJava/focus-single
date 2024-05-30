1d9d9c7cf10719f4e3a38511ac1257f575c73743
菜哥 <745876299@qq.com>
2024-05-30 13:51:38 +0800

    注释改动

--- internal/logic/setting/setting.go
+++ internal/logic/setting/setting.go
@@ -20,7 +20,7 @@ func New() *sSetting {
 	return &sSetting{}
 }
 
-// 设置KV。
+// Set 设置KV。
 func (s *sSetting) Set(ctx context.Context, key, value string) error {
 	_, err := dao.Setting.Ctx(ctx).Data(entity.Setting{
 		K: key,
@@ -29,13 +29,13 @@ func (s *sSetting) Set(ctx context.Context, key, value string) error {
 	return err
 }
 
-// 查询KV。
+// Get 查询KV。
 func (s *sSetting) Get(ctx context.Context, key string) (string, error) {
 	v, err := s.GetVar(ctx, key)
 	return v.String(), err
 }
 
-// 查询KV，返回泛型，便于转换。
+// GetVar 查询KV，返回泛型，便于转换。
 func (s *sSetting) GetVar(ctx context.Context, key string) (*g.Var, error) {
 	v, err := dao.Setting.Ctx(ctx).Fields(dao.Setting.Columns().V).Where(dao.Setting.Columns().K, key).Value()
 	return v, err
