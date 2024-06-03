095db3fbcdddbf09a7759aed57b094b8db095518
菜哥 <745876299@qq.com>
2024-06-03 14:59:43 +0800

    性别获取改动

--- internal/consts/user.go
+++ internal/consts/user.go
@@ -3,16 +3,17 @@ package consts
 const (
 	UserStatusOk       = 0 // 用户状态正常
 	UserStatusDisabled = 1 // 用户状态禁用
-	UserGenderUnknown  = 3 // 性别: 未知
 	UserGenderMale     = 1 // 性别: 男
 	UserGenderFemale   = 2 // 性别: 女
+	UserGenderUnknown  = 3 // 性别: 未知
 	UserLoginUrl       = "/login"
 )
 
 func GetGenderByType(typeInt int) string {
 	dataMap := map[int]string{
-		UserGenderMale:   "男",
-		UserGenderFemale: "女",
+		UserGenderMale:    "男",
+		UserGenderFemale:  "女",
+		UserGenderUnknown: "未知",
 	}
 	if content, exists := dataMap[typeInt]; exists {
 		return content
